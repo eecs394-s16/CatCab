25a94db58fbbbeaccba2e13d9276aa7e58d54029
kmads <kamaddio@gmail.com>
2016-04-03 17:57:27 -0500

    Front end form and firebase database connected
    You can now add name, phone, and terminal on the home page form, and
    click “Create New User” button to add them to the database. The
    database updates immediately with the new user and their fields.
    
    First names and terminals are displayed on the home screen now for
    debugging purposes

--- catcab/app/users/scripts/IndexController.js
+++ catcab/app/users/scripts/IndexController.js
@@ -2,10 +2,21 @@ angular
   .module('users')
   .controller("IndexController", ["$scope", "$firebaseArray",
     function ($scope, $firebaseArray) {
-    //create connection with the db
-    var ref = new Firebase("https://catcab.firebaseio.com/");
+    //create connection with the db, put users into the "users" folder on firebase
+    var ref = new Firebase("https://catcab.firebaseio.com/users");
      // download the data into a local object
     $scope.users = $firebaseArray(ref);
+
+    // add a new user to the database when a new person fills out the form 
+    $scope.addUser = function() {
+    $scope.users.$add({
+      firstName: $scope.firstName,
+      lastName: $scope.lastName,
+      phone: $scope.phone,
+      terminal: $scope.terminal
+    });
+  };
+
   }]);
 
   // , supersonic
