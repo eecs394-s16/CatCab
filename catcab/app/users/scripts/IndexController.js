85715bef024f9ac975453823f1426c5ba476966f
kmads <kamaddio@gmail.com>
2016-04-04 15:47:07 -0500

    Photo uploading and displaying locally, photo display from hard-coded database
    This code has two parts:
    1. User can choose to upload a photo from their phone, and the app will
    display it on the page.
    2. In the user loop, we now display name, terminal, and imgSrc.
    Currently, I hard-coded a base64 string as a field for one of the users
    in the firebase browser screen, so one image should display next to the
    name of the person who uploaded that image.
    
    TODO:
    1. After a user uploads a photo, send the base64 string ($scope.imgSrc)
    to the database in the user’s imgSrc field.
    2. After a pair of users is matched, display the user’s match’s photo
    on the user’s screen so they know who to look for. (Access via
    something like user.matchId.imgSrc)

--- catcab/app/users/scripts/IndexController.js
+++ catcab/app/users/scripts/IndexController.js
@@ -7,6 +7,9 @@ angular
      // download the data into a local object
     $scope.users = $firebaseArray(ref);
 
+    $scope.image = null;
+    $scope.imgData = null;
+
     // add a new user to the database when a new person fills out the form 
     $scope.addUser = function() {
     $scope.users.$add({
@@ -16,6 +19,41 @@ angular
       terminal: $scope.terminal
     });
   };
+
+  	// PHOTO UPLOADING/DISPLAYING CODE
+  	// Will go into the image tag once user uploades a photo
+	 $scope.imgSrc = null;
+  
+	// Note/Feature to add: Allow users to use camera to take a current photo of themselves using front-cam?
+
+	// Get the image from the user's camera roll
+	$scope.getImage = function() {
+		var options = {
+			quality: 50,
+			allowEdit: true,
+			encodingType: "png",
+			sourceType: 0,
+			// destinationType: navigator.camera.DestinationType.FILE_URI, // for image URI method
+			destinationType: navigator.camera.DestinationType.DATA_URL, // for base64 method
+			targetWidth: 300
+		};
+		navigator.camera.getPicture(onCameraSuccess, onCameraFail, options);
+		$scope.$apply();
+	};    
+    
+	// Uses the imageData (base64 string) to create a full image source for the HTML img tag
+    function onCameraSuccess(imageData) {
+        $scope.imgSrc = "data:image/png;base64,"+imageData;
+        $scope.imgData = imageData;
+        $scope.$apply();
+    }
+
+    // Displays a pop-up message if either no photo is selected, or another error occurs
+    function onCameraFail(message) {
+        alert('Failed because: ' + message);
+        $scope.$apply();
+	}
+
 
   }]);
 
