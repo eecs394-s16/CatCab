644bc4f135310ce83b2aec5db9e7556f6e8c9262
Bruno Peynetti <bpeynetti@u.northwestern.edu>
2016-04-03 20:02:59 -0500

    Debugging matching 1

--- catcab/app/users/scripts/IndexController.js
+++ catcab/app/users/scripts/IndexController.js
@@ -1,3 +1,6 @@
+var myId;
+var matched = false;
+
 angular
   .module('users')
   .controller("IndexController", ["$scope", "$firebaseArray",
@@ -13,8 +16,66 @@ angular
       firstName: $scope.firstName,
       lastName: $scope.lastName,
       phone: $scope.phone,
-      terminal: $scope.terminal
+      terminal: $scope.terminal,
+      matchId: "no one"
+    }).then(function(ref) {
+      myId = ref.key();
+      supersonic.logger.log("My key is "+myId);
     });
+    $scope.users.$watch(function(event) {
+      supersonic.logger.log(event);
+      if (matched){
+        return;
+      }
+      var myRecord = $scope.users.$getRecord(myId);
+
+      if (event.event==="child_changed" && event.key===myId)
+      {
+        //someone matched with us
+        var matchRecord = $scope.users.$getRecord(myRecord.matchId)
+        $scope.match = {
+          firstName: matchRecord.firstName,
+          lastName: matchRecord.lastName,
+          phone: matchRecord.phone,
+          terminal: matchRecord.terminal,
+          matchId: matchRecord.matchId
+        };
+        matched = true;
+
+
+      }
+      else if(event.event==="child_added" && event.key!==myId){
+
+        var newRecord = $scope.users.$getRecord(event.key);
+        var terminal = newRecord.terminal;
+        if (terminal === $scope.terminal && (newRecord.matchId==="no one"))
+        {
+          //found a match
+
+          //change the newRecord match 
+          newRecord.matchId = myId;
+
+          //change my own match 
+          myRecord.matchId = newRecord.getKey();
+
+          $scope.match = {
+            firstName: newRecord.firstName,
+            lastName: newRecord.lastName,
+            phone: newRecord.phone,
+            terminal: newRecord.terminal,
+            matchId: newRecord.matchId
+          };
+
+
+          $save(newRecord);
+          $save(myRecord);
+          matched = true;
+        }
+
+      }
+
+    })
+
   };
 
   }]);
