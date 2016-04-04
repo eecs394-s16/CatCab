e49a59f41b0f981936d20a0500953b7089c15bed
paep3nguin <paep3nguin@gmail.com>
2016-04-03 20:23:30 -0500

    DB Matching works

--- catcab/app/users/scripts/IndexController.js
+++ catcab/app/users/scripts/IndexController.js
@@ -21,60 +21,55 @@ angular
     }).then(function(ref) {
       myId = ref.key();
       supersonic.logger.log("My key is "+myId);
-    });
-    $scope.users.$watch(function(event) {
-      supersonic.logger.log(event);
-      if (matched){
-        return;
-      }
-      var myRecord = $scope.users.$getRecord(myId);
 
-      if (event.event==="child_changed" && event.key===myId)
-      {
-        //someone matched with us
-        var matchRecord = $scope.users.$getRecord(myRecord.matchId)
-        $scope.match = {
-          firstName: matchRecord.firstName,
-          lastName: matchRecord.lastName,
-          phone: matchRecord.phone,
-          terminal: matchRecord.terminal,
-          matchId: matchRecord.matchId
-        };
-        matched = true;
+      $scope.users.$watch(function(event) {
+        supersonic.logger.log(event);
+        if (matched){
+          return;
+        }
+        var myRecord = $scope.users.$getRecord(myId);
 
-
-      }
-      else if(event.event==="child_added" && event.key!==myId){
-
-        var newRecord = $scope.users.$getRecord(event.key);
-        var terminal = newRecord.terminal;
-        if (terminal === $scope.terminal && (newRecord.matchId==="no one"))
+        if (event.event==="child_changed" && event.key===myId)
         {
-          //found a match
-
-          //change the newRecord match 
-          newRecord.matchId = myId;
-
-          //change my own match 
-          myRecord.matchId = newRecord.getKey();
-
+          //someone matched with us
+          var matchRecord = $scope.users.$getRecord(myRecord.matchId)
           $scope.match = {
-            firstName: newRecord.firstName,
-            lastName: newRecord.lastName,
-            phone: newRecord.phone,
-            terminal: newRecord.terminal,
-            matchId: newRecord.matchId
+            firstName: matchRecord.firstName,
+            lastName: matchRecord.lastName,
+            phone: matchRecord.phone,
+            terminal: matchRecord.terminal,
+            matchId: matchRecord.matchId
           };
-
-
-          $save(newRecord);
-          $save(myRecord);
           matched = true;
         }
+        else if(event.event==="child_added" && event.key!==myId){
+          var newRecord = $scope.users.$getRecord(event.key);
+          var terminal = newRecord.terminal;
+          if (terminal === $scope.terminal && (newRecord.matchId==="no one"))
+          {
+            //found a match
 
-      }
+            //change the newRecord match 
+            newRecord.matchId = myId;
 
-    })
+            //change my own match
+            myRecord.matchId = newRecord.$id;
+
+            $scope.match = {
+              firstName: newRecord.firstName,
+              lastName: newRecord.lastName,
+              phone: newRecord.phone,
+              terminal: newRecord.terminal,
+              matchId: newRecord.matchId
+            };
+
+            $scope.users.$save(newRecord);
+            $scope.users.$save(myRecord);
+            matched = true;
+          }
+        }
+      });
+    });
 
   };
 
