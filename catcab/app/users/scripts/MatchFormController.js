e7153901177c4fc2c737dbec20e519aa19f28d60
kmads <kamaddio@gmail.com>
2016-04-20 10:12:03 -0500

    "Find Match" now adds user's newest trip information under their /matches array in firebase

--- catcab/app/users/scripts/MatchFormController.js
+++ catcab/app/users/scripts/MatchFormController.js
@@ -29,6 +29,35 @@ angular
       // This variable stores the combined datetime info we will use for the match
       $scope.datetime = new Date();
 
+      // Get user's info from local storage
+      // $scope.phone = localStorage.getItem("phoneNumber");
+      // $scope.firstName = localStorage.getItem("firstName");
+      // $scope.lastName = localStorage.getItem("lastName");
+      // $scope.imgSrc = localStorage.getItem("imgSrc");
+
+      // Hard coded value for now
+      $scope.phone = "8476368102";
+      var user_ref = new Firebase("https://catcab.firebaseio.com/users/"+$scope.phone);
+      var user_matches_ref = new Firebase("https://catcab.firebaseio.com/users/"+$scope.phone+"/matches");
+      $scope.findMatch = function(){
+
+        // Leave datetime empty if they are looking for a match now
+        if ($scope.leaveNowLater == "now"){
+          $scope.datetime = "";
+        }
+
+        // Push this trip data to the users/<phoneNumber>/matches array
+        trip_data = ({
+          destination: $scope.destination,
+          origin: $scope.origin,
+          time: "" + String($scope.datetime),
+          type: $scope.leaveNowLater,
+          myMatch: ""
+        });
+
+        user_matches_ref.push(trip_data);
+
+    }
 
       // This combines the selected date and selected time into a single datetime object
     $scope.combineDateWithTime = function()
