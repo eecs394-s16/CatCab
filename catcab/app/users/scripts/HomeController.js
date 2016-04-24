376369da40b2237ce730eff0ec9e20053f4119e2
paep3nguin <paep3nguin@gmail.com>
2016-04-24 00:09:40 -0500

    Home page cancellation works, login redirects to home immediately if logged in

--- catcab/app/users/scripts/HomeController.js
+++ catcab/app/users/scripts/HomeController.js
@@ -3,31 +3,36 @@ angular
 	.controller("HomeController", ["$scope", "$firebaseArray","$firebaseObject",
 		function($scope, $firebaseArray, $firebaseObject) {
 		
-		view = new supersonic.ui.View("users#home");
-		view.start("home").then( function(startedView) {
-		  supersonic.ui.layers.replace(startedView);
-		});
+		// view = new supersonic.ui.View("users#home");
+		// view.start("home").then(function (startedView) {
+	 //  		supersonic.ui.layers.replace(startedView);
+		// });
 
-			$scope.phone = localStorage.getItem("phoneNumber");
-			// var ref = new Firebase("https://catcab.firebaseio.com/users");
-			// $scope.users = $firebaseArray(ref);
+		$scope.phone = localStorage.getItem("phoneNumber");
 
-			var ref1 = new Firebase("https://catcab.firebaseio.com/users/" + $scope.phone);
-			$scope.userme = $firebaseObject(ref1);
+		var ref1 = new Firebase("https://catcab.firebaseio.com/users/" + $scope.phone);
+		$scope.userme = $firebaseObject(ref1);
 
-			$scope.cancel_ride = function(v) {
-				// Not Working Yet
-				var newuser = new Firebase("https://catcab.firebaseio.com/users/"+$scope.phone+"/"+v);
-				newuser.update({ status: 'cancelled'});
-			}
+		$scope.cancel_ride = function(v) {
+			$scope.userme.matches[v].status = 'cancelled';
+			$scope.userme.$save();
+		};
 
-			$scope.decline_ride = function(v) {
-				// Not Working Yet
-				var newuser = new Firebase("https://catcab.firebaseio.com/users/"+$scope.phone+"/"+v);
-				newuser.update({ status: 'waiting'});
-			}
+		$scope.get_user = function(id) {
+			var userRef = new Firebase("https://catcab.firebaseio.com/users/"+id);
+			return $firebaseObject(userRef);
+		};
 
-		}
-
-
-	]);
+		$scope.pretty_time = function(timeStr) {
+			var date = new Date(timeStr);
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			var ampm = hours >= 12 ? 'pm' : 'am';
+			hours = hours % 12;
+			hours = hours ? hours : 12; // the hour '0' should be '12'
+			minutes = minutes < 10 ? '0'+ minutes : minutes;
+			var time = hours + ':' + minutes + ' ' + ampm;
+			return date.toDateString() + " at " + time;
+		};
+	}
+]);
--- catcab/app/users/scripts/LoginController.js
+++ catcab/app/users/scripts/LoginController.js
@@ -1,68 +1,57 @@
-var myPhoneNumber = 0
-supersonic.logger.log("Outside angular")
-
 angular
 	.module('users')
 	.controller("LoginController", ["$scope", "$firebaseArray","$firebaseObject",
 		function($scope, $firebaseArray, $firebaseObject) {
 
+			if (localStorage.getItem("phoneNumber") !== null) {
+				supersonic.ui.views.start("users#home").then(function (startedView) {
+			  		supersonic.ui.layers.replace(startedView);
+				});
+			}
+
 			$scope.badLogin = false;
 
-			// var ref = new Firebase("https://catcab.firebaseio.com/users");
-			// $scope.users = $firebaseArray(ref);
-
-			$scope.phone_num = 0;
 			// supersonic.logger.log("HI there!"+$scope.phone);
 
 			$scope.login_user = function() {
-
-				$scope.phone_num = $scope.phone;
-				localStorage.setItem("phoneNumber", $scope.phone );
 				// supersonic.logger.log("Phone: "+localStorage.getItem("phoneNumber"));
 
-				var testRef = new Firebase("https://catcab.firebaseio.com/users/"+$scope.phone_num);
-				var obj = new $firebaseObject(testRef);
+				var userRef = new Firebase("https://catcab.firebaseio.com/users/"+phone);
+				var obj = new $firebaseObject(userRef);
 				obj.$loaded().then(function() {
-						obj = obj.$value;
-					});
-				var myRecord = obj;
-				// var myRecord = $scope.users.$getRecord($scope.phone_num);
+					if (obj === null) {
+						// user not in database, 
+						// display a message saying "not in database"
+						// and show a sign-up button
+						$scope.badLogin = true;
+					}
+					else
+					{
+						// got the user approved
 
-				if (myRecord === null){
-					// user not in database, 
-					// display a message saying "not in database"
-					// and show a sign-up button
-					$scope.badLogin = true;
-				}
-				else
-				{
-					// got the user approved 
+						// save local storage
+						supersonic.logger.log("Name is "+obj.firstName);
 
-					// save local storage
-					supersonic.logger.log("Name is "+myRecord.firstName);
+						localStorage.setItem("firstName",obj.firstName);
 
-					localStorage.setItem("firstName",myRecord.firstName);
+						localStorage.setItem("lastName", obj.lastName);
 
-					localStorage.setItem("lastName", myRecord.lastName);
+						localStorage.setItem("phoneNumber", phone);
 
-					localStorage.setItem("phoneNumber",$scope.phone);
+						localStorage.setItem("imgSrc", obj.imgSrc);
 
-					localStorage.setItem("imgSrc", myRecord.imgSrc);
-
-					localStorage.setItem("matches", myRecord.matches);
+						localStorage.setItem("matches", obj.matches);
 
 
-					// move to home view
-
-					var view = new supersonic.ui.View("users#home");
-					var customAnimation = supersonic.ui.animate("flipVerticalFromBottom");
-					supersonic.ui.layers.push(view, { animation: customAnimation });
-
-				}
-
-			}; 
-
-
+						// move to home view
+						// var view = new supersonic.ui.View("users#home");
+						// var customAnimation = supersonic.ui.animate("flipVerticalFromBottom");
+						// supersonic.ui.layers.push(view, { animation: customAnimation });
+						supersonic.ui.views.start("users#home").then(function (startedView) {
+					  		supersonic.ui.layers.replace(startedView);
+						});
+					}
+				});
+			};
 		}
-
 	]);
